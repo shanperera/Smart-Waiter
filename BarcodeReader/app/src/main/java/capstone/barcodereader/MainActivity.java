1260098968710dcd1a86ca8388e6fd091f10c101
pererasp <pererali@mcmaster.ca>
2015-11-13 19:36:12 -0500

    QR/Barcode scanner functionality added
    completed basic functionality of QR/barcode reader, when button pressed

--- BarcodeReader/app/src/main/java/capstone/barcodereader/MainActivity.java
+++ BarcodeReader/app/src/main/java/capstone/barcodereader/MainActivity.java
@@ -1,12 +1,21 @@
 package capstone.barcodereader;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button scanButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +23,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
+        // Created an onClickListener for the "Scan QR code / Barcode" button
+        // When clicked, this activity initialises an IntentIntegrator, a class
+        // defined by the ZXing embedded scanner library to initialise the
+        // barcode scanner, initiateScan() brings up the local camera app
+        // and prompts the user to take a picture of the QR/barcode
+
+        scanButton = (Button)findViewById(R.id.scanCodeButton);
+        scanButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                IntentIntegrator integrator = new IntentIntegrator(MainActivity.this);
+                integrator.initiateScan();
+            }
+        });
 
     }
 
@@ -39,4 +64,17 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    // onActivityResult is the function that is called when the code scanner
+    // is given a QR/barcode to read. onActivityResult handles the scan result
+    // and stores it in the IntentResult variable. For now the function only
+    // prints the information passed through the code to the Log. Further
+    // functionality will be added as development progresses
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+        if (scanResult != null) {
+            String re = scanResult.getContents();
+            Log.d("code", re);
+        }
+
+    }
 }
