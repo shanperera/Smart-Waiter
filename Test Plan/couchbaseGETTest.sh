3f5fb304ed4dc2d1536b0d1d158683b0f6a1ea73
merajpatel <patelmu2@mcmaster.ca>
2016-04-17 01:03:52 -0400

    added script for database test, modified test plan

---
+++ Test Plan/couchbaseGETTest.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Run script: ./couchbaseGETTest <iterations>
+UserValue=$1
+
+# loop forever
+while true
+do
+  # start threads
+  for i in $(seq 1 $UserValue)
+  do
+    echo "started instance no: $i"
+    curl -X GET -H "Content-Type: application/text" "http://administrator:administrator@localhost:8091/pools/default/buckets/couchbaseevents/docs/789"
+  done
+  # sleep 2 min
+  sleep 120
+done
